3b990297560e7cbf2ef086873ef089a475c757ab
Niteowl <joseovab@gmail.com>
2023-10-22 15:33:11 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Tarea1
+using System;
+using System.Diagnostics;
+using System.Text;
+
+Random random = new Random();
+int cantidadDeTarjetas = 1000;
+
+for (int i = 0; i < cantidadDeTarjetas; i++)
+{
+    string numeroDeTC = GenerarNumeroDeTarjeta();
+    Console.WriteLine(numeroDeTC);
+    ProcesarTarjetas();
+}
+Stopwatch sw = Stopwatch.StartNew();
+
+Console.WriteLine("Procesando pago, por favor espere...");
+
+//for (int i = 0; i < cantidadDeTarjetas; i++)
+//{
+//    ProcesarTarjetas();
+//}
+
+await Task.Delay(10000);
+
+sw.Stop();
+
+Console.WriteLine($"Todos los pagos fueron procesados: {TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds).TotalSeconds} segundos");
+
+Console.WriteLine("Programa finalizado.");
+
+string GenerarNumeroDeTarjeta()
+{
+    string numeroDeTC="4";
+    for (int i = 0; i < 15; i++)
+    {
+        numeroDeTC = numeroDeTC + random.Next(10); // Genera un dígito aleatorio
+    }
+    return numeroDeTC;
+}
+
+async Task ProcesarTarjetas()
+{
+    await Task.Delay(10000);
+}
+
+
