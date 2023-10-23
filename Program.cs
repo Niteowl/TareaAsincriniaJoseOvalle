8f74cbd8300f7827e62b07e16607a9d11f6063f1
Niteowl <joseovab@gmail.com>
2023-10-23 16:02:54 -0300

    tarea

--- Program.cs
+++ Program.cs
@@ -1,47 +1,23 @@
-﻿// Tarea1
+﻿// Tarea2
 using System;
 using System.Diagnostics;
 using System.Text;
 
 Random random = new Random();
-int cantidadDeTarjetas = 1000;
+string nombre = "";
+Console.WriteLine("Descarga informacion de usuarios: ");
 
-for (int i = 0; i < cantidadDeTarjetas; i++)
+while (true)
 {
-    string numeroDeTC = GenerarNumeroDeTarjeta();
-    Console.WriteLine(numeroDeTC);
-    ProcesarTarjetas();
-}
-Stopwatch sw = Stopwatch.StartNew();
-
-Console.WriteLine("Procesando pago, por favor espere...");
-
-//for (int i = 0; i < cantidadDeTarjetas; i++)
-//{
-//    ProcesarTarjetas();
-//}
-
-await Task.Delay(10000);
-
-sw.Stop();
-
-Console.WriteLine($"Todos los pagos fueron procesados: {TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds).TotalSeconds} segundos");
-
-Console.WriteLine("Programa finalizado.");
-
-string GenerarNumeroDeTarjeta()
-{
-    string numeroDeTC="4";
-    for (int i = 0; i < 15; i++)
-    {
-        numeroDeTC = numeroDeTC + random.Next(10); // Genera un dígito aleatorio
-    }
-    return numeroDeTC;
+    Console.WriteLine("\nIngrese Nombre");
+    nombre = Console.ReadLine();
+    _ = DescargaDeDatos(nombre);
 }
 
-async Task ProcesarTarjetas()
+async Task DescargaDeDatos(string nombreEntrada)
 {
-    await Task.Delay(10000);
+    int tiempoEspera = random.Next(10000, 60000);
+    await Task.Delay(tiempoEspera);
+    tiempoEspera = tiempoEspera / 1000;
+    Debug.WriteLine($"Información de {nombreEntrada} descargada con éxito en aprox {tiempoEspera} seg");
 }
-
-
